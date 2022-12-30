bef4465d3949524b40db01a1d116bc9283b62102
apocalypse1666 <121695362+apocalypse1666@users.noreply.github.com>
2022-12-31 00:17:11 +0300

    Add files via upload

---
+++ PERFECT NUMBERS O.O.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PERFECT_NUMBERS_5_O.O
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, n1, n2, sum = 0;
+            Console.WriteLine("First Number");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Second Number");
+            n2 = int.Parse(Console.ReadLine());
+            n = n1 + 1;
+            while (n < n2)
+            {
+                for (int i = 1; i <= n / 2; i++)
+                {
+                    if ((n % i) == 0)
+                        sum += i;
+                }
+                if (sum == n)
+                    Console.WriteLine(n);
+                sum = 0;
+                n++;
+            }
+        }
+    }
+}
---
+++ PRIME NUMBERS O.O.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRIME_NUMBERS_O.O
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, n1, n2, sum = 0;
+            Console.WriteLine("enter first Number");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter last Number");
+            n2 = int.Parse(Console.ReadLine());
+            n = n1 + 1;
+            while (n < n2)
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    if ((n % i) == 0)
+                        sum += i;
+                }
+                if (sum == n + 1)
+                    Console.WriteLine(n);
+                sum = 0;
+                n++;
+            }
+        }
+    }
+}
